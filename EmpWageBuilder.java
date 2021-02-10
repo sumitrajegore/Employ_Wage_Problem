ebcb78922d45a04248db8ef00233652ce8224b46
sumitrajegore <sumitrajegorevp@gmail.com>
2021-02-10 21:06:20 +0530

    ADDED : employ present or absent feature

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,7 +1,13 @@
 public class EmpWageBuilder {
 
       public static void main(String[] args) {
-
+       
+           int IS_FULL_TIME = 1;
            System.out.println("\n welcome to employ wage program \n");
+		double empCheck = Math.floor(Math.random() * 10 ) % 2 ;
+		if (IS_FULL_TIME == empCheck)
+		     System.out.println("Employee is Present");
+		else
+	             System.out.println("Employee is Absent");
      }
 }
