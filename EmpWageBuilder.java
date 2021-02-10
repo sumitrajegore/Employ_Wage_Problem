b30870a16bbd72678627251ea694c76c982a5202
sumitrajegore <sumitrajegorevp@gmail.com>
2021-02-10 21:24:20 +0530

    ADDED : calculate employ Wage for fulltime

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,13 +1,17 @@
 public class EmpWageBuilder {
 
       public static void main(String[] args) {
-       
+
            int IS_FULL_TIME = 1;
-           System.out.println("\n welcome to employ wage program \n");
-		double empCheck = Math.floor(Math.random() * 10 ) % 2 ;
-		if (IS_FULL_TIME == empCheck)
-		     System.out.println("Employee is Present");
-		else
-	             System.out.println("Employee is Absent");
+           int EMP_RATE_PER_HOUR = 20;
+           int empHrs = 0;
+           int empWage = 0;
+	   double empCheck = Math.floor(Math.random() * 10 ) % 2 ;
+           if (empCheck == IS_FULL_TIME)
+	       empHrs = 8;
+	   else
+               empHrs = 0;
+           empWage = empHrs * EMP_RATE_PER_HOUR;
+           System.out.println("Emp Wage : " + empWage);
      }
 }
